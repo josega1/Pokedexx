bbd97d80f3bd35ff7e11fdacd0efe7a10b1bbd9c
BaironSandoval <baironalfonsosandovalnino@gmail.com>
2023-05-30 15:42:46 -0500

    styles-home

--- src/components/home/UserNameForm/UserNameForm.jsx
+++ src/components/home/UserNameForm/UserNameForm.jsx
@@ -1,36 +1,43 @@
-import { useRef, useState } from 'react'
-import './UserNameForm.css'
+import { useRef, useState } from "react";
+import "./UserNameForm.css";
 
 const UserNameForm = ({ onSendName }) => {
-  const [userNameValue, setUserNameValue] = useState('');
-  const [nameError, setNameError] = useState('');
+  const [userNameValue, setUserNameValue] = useState("");
+  const [nameError, setNameError] = useState("");
   const hasInputAlreadyTouched = useRef(false);
 
   const handleChange = (e) => {
     const nameValue = e.target.value;
-    if(!hasInputAlreadyTouched.current) hasInputAlreadyTouched.current = true;
+    if (!hasInputAlreadyTouched.current) hasInputAlreadyTouched.current = true;
 
-    if(!nameValue) setNameError('El nombre está vacío!');
-    else if(/[^a-z ]/i.test(nameValue)) setNameError('Solo se permiten letras y espacios.');
-    else if(!/^[a-z]{2,} ?$/i.test(nameValue)) setNameError('El nombre debe tener mínimo dos letras');
-    else setNameError('');
-    
+    if (!nameValue) setNameError("El nombre está vacío!");
+    else if (/[^a-z ]/i.test(nameValue))
+      setNameError("Solo se permiten letras y espacios.");
+    else if (!/^[a-z ]{2,} ?$/i.test(nameValue))
+      setNameError("El nombre debe tener mínimo dos letras");
+    else setNameError("");
+
     setUserNameValue(nameValue);
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    if(!nameError && hasInputAlreadyTouched.current) onSendName(userNameValue);
+    if (!nameError && hasInputAlreadyTouched.current) onSendName(userNameValue);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-        {Boolean(nameError) && <p>{nameError}</p> }
-        <input type="text" placeholder='Tu nombre... ' value={userNameValue} onChange={handleChange}/>
-        <button type='submit'>Comenzar</button>
+      {Boolean(nameError) && <p>{nameError}</p>}
+      <input
+        type="text"
+        placeholder="Tu nombre... "
+        value={userNameValue}
+        onChange={handleChange}
+      />
+      <button type="submit"><img src="https://i.pinimg.com/736x/75/b0/72/75b0728c429ced949eb0e9287f7c9a95.jpg" alt="comenzar" /></button>
     </form>
-  )
-}
+  );
+};
 
-export default UserNameForm
+export default UserNameForm;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,14 +17,20 @@ const Home = () => {
     navigate(from);
   }
   return (
-    <section>
-      <div className="logo-container">
+    <section className='container_home'>
+      <div>
+        <div className="logo-container">
         <img src="https://s3-alpha-sig.figma.com/img/ca59/d9ce/98042af437fdff212d3259040db2e2db?Expires=1685923200&Signature=nMY1KszWu1~qq7kxGEJRphghbXXvBvk33QotdeoaHfrIMQ8SeDK~5lLdVz-3ujOHlZooSwxX8YPNCciZ3nX8lvIU4WCSLeeZj-h-KAYc~Ne33A3-rjiyRaMfRDzeLJ0XRN6YrkgNiiPHMb-Yu-p0d0h7nr1X7dBwUJKgy943Z~LUuGWS4tV7OkNz4Cf7BrIt2SVqPH00e8rTh44igGTaalgTSgKZU9XFe~DprWjxWc7owZcOYhJO9l88xicwoCjlAz4RytbcQhgBzrAUsBce0VkmAsH3q0XRDTjWCA5t7ed95QJUh0kw9QmQEogeQilFaiUxqVJtfk9VJC4cNYzJ9Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="Pokedex" />
       </div>
       <h1 className='home__title'>¡Hola entrenador!</h1>
       <p className='home__description'>Para poder comenzar, dame tu nombre</p>
       <div className='home__form-container'>
         <UserNameForm onSendName={handleSendName}/>
+      </div>
+      </div>
+      
+      <div className='container-giphy-embed'>
+        <iframe src="https://giphy.com/embed/DJM88aCmEeaNG" className="giphy-embed" allowFullScreen></iframe>
       </div>
       
     </section>
