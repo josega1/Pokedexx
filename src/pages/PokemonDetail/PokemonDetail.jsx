2a5288fbb90117cc7759055d0dd8532a8d6bb83a
unknown <jr747524@gmail.com>
2023-05-31 18:41:29 -0500

    css details

--- src/pages/PokemonDetail/PokemonDetail.jsx
+++ src/pages/PokemonDetail/PokemonDetail.jsx
@@ -1,13 +1,78 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { getPokemonById } from "../../services/getPokemonById"
 import './PokemonDetail.css'
 
+const statsDetail = ['Hp', 'attack', 'defense', 'speed', "special-attack", "special-defense"];
+
 const PokemonDetail = () => {
-  const {pokemonId} = useParams()  
+  const {pokemonId} = useParams() 
+  const [pokemonDetail, setPokemonDetail] = useState(null) 
+  useEffect(() => {
+    const loadPokemon = async () => {
+      const pokemon = await getPokemonById(pokemonId);
+      setPokemonDetail(pokemon);
+    }
+
+    loadPokemon()
+  }, [])
+
+  const stats = pokemonDetail?.stats.filter(stat => statsDetail.includes(stat.name.toLowerCase())); 
+  
 
   return (
-    <div className="container_detail">
-        <h1>Pokemon Detail</h1> 
-        <p>Aqui se evidenciara mas detalles sobre el pokemon con el ID: {pokemonId}</p>
+    <div className="container-detail">
+        
+        {!pokemonDetail && <p>Loading ... </p>}
+        {pokemonDetail && (
+            <>
+              <h1>Pokemon Detail</h1> 
+              <p>Aqui se evidenciara mas detalles sobre el pokemon con el ID: {pokemonId}</p>
+              <div className="pokemondetail-card__img">
+                <img src={pokemonDetail.image} alt={pokemonDetail.name} />
+              </div>
+              <div className="container__pokemondetail-card">
+                <h2 className="pokemondetail-card__title">{pokemonDetail.name}</h2>
+              </div>
+              <div>
+                <h3>Height: {pokemonDetail.height}</h3>
+                <h3>Weight: {pokemonDetail.weight}</h3>
+              </div>
+              <section>
+                <h3>Tipo</h3>
+                <ul className="pokemondetail-card__list">
+                    {pokemonDetail.types.map((type) => (
+                        <li key={type} className="pokemon-card__item">
+                            {type}
+                        </li>
+                    ))}
+                </ul>
+              </section>
+
+              <section>
+                <h3>Abilities</h3>
+                <ul className="pokemondetail-card__list">
+                    {pokemonDetail.abilities.map((ability) => (
+                        <li key={ability} className="pokemon-card__item">
+                            {ability}
+                        </li>
+                    ))}
+                </ul>
+              </section>
+
+              <section>
+                <h3>Stats</h3>
+                <ul className="pokemondetail-card__list">
+                    {stats.map((stat) => (
+                        <li key={stat.name} className="pokemon-detail-card__item">
+                            <em>{stat.name.toUpperCase()}</em>
+                            <span>{stat.value}</span>
+                        </li>
+                    ))}
+                </ul>
+              </section>
+            </>
+        )}
     </div>
   )
 }
--- src/services/getPokemonById.js
+++ src/services/getPokemonById.js
@@ -3,17 +3,11 @@ import { axiosInstance } from "../api/axiosInstance";
 export const getPokmonImg = (sprites) => {
 
     const firstOption = sprites.other.dream_world.front_default;
-
     const secondOption = sprites.other['official-artwork'].front_default;
-
     const thirdOption = sprites.other.home.front_default;
 
- 
-
     if (firstOption) return firstOption;
-
     if (secondOption) return secondOption;
-
     if (thirdOption) return thirdOption;
 }
 
@@ -30,6 +24,9 @@ export const getPokemonById = async (id) => {
             value: statInfo.base_stat,
           })),
           image: getPokmonImg(pokemonData.sprites),
+          abilities: pokemonData.abilities.map(abilityInfo => abilityInfo.ability.name),
+          height: pokemonData.height,
+          weight: pokemonData.weight,
         };
         return adaptedPokemon;
     } catch (error) {
