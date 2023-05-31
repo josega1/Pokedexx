146a5b1a3d48136c09b46626532025d56410c44e
christian alzate <chris-aav@hotmail.com>
2023-05-31 17:15:54 -0500

    fondoDetail

--- src/pages/PokemonDetail/PokemonDetail.jsx
+++ src/pages/PokemonDetail/PokemonDetail.jsx
@@ -1,10 +1,11 @@
 import { useParams } from "react-router-dom"
+import './PokemonDetail.css'
 
 const PokemonDetail = () => {
   const {pokemonId} = useParams()  
 
   return (
-    <div>
+    <div className="container_detail">
         <h1>Pokemon Detail</h1> 
         <p>Aqui se evidenciara mas detalles sobre el pokemon con el ID: {pokemonId}</p>
     </div>
