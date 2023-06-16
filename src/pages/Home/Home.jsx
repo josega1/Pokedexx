8982abf2c1441c8f4c2e0b3d0026b9cd26c5ebf2
unknown <jr747524@gmail.com>
2023-06-16 09:13:47 -0500

    version5

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { UserNameContext } from "../../context/UserNameContext";
 import { useLocation, useNavigate } from "react-router-dom";
 import pokeball from "../../images/giphy.gif";
+import pokedex from '../../images/Imagen1.png'
 import UserNameForm from "../../components/home/UserNameForm/UserNameForm";
 
 import "./Home.css";
@@ -25,7 +26,7 @@ const Home = () => {
         <div className="container-info">
           <div className="logo-container">
             <img
-              src="https://s3-alpha-sig.figma.com/img/ca59/d9ce/98042af437fdff212d3259040db2e2db?Expires=1685923200&Signature=nMY1KszWu1~qq7kxGEJRphghbXXvBvk33QotdeoaHfrIMQ8SeDK~5lLdVz-3ujOHlZooSwxX8YPNCciZ3nX8lvIU4WCSLeeZj-h-KAYc~Ne33A3-rjiyRaMfRDzeLJ0XRN6YrkgNiiPHMb-Yu-p0d0h7nr1X7dBwUJKgy943Z~LUuGWS4tV7OkNz4Cf7BrIt2SVqPH00e8rTh44igGTaalgTSgKZU9XFe~DprWjxWc7owZcOYhJO9l88xicwoCjlAz4RytbcQhgBzrAUsBce0VkmAsH3q0XRDTjWCA5t7ed95QJUh0kw9QmQEogeQilFaiUxqVJtfk9VJC4cNYzJ9Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+              src={pokedex}
               alt="Pokedex"
             />
           </div>
